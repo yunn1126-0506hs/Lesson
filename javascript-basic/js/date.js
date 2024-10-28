3dd761634e3a6ca824bd165202379289972cabbd
yunn1126-0506hs <tko38ksmy38@gmail.com>
2024-10-28 22:20:01 +0900

    Add files via upload

---
+++ javascript-basic/js/date.js
@@ -0,0 +1,20 @@
+
+// // // // // 教材より戻り値の使い方
+
+// 与えられた引数numを２倍にし、その値を戻り値として返す関数を定義する
+const double = (num) =>{
+  return num*2;
+}
+// 関数の戻り値を出力する
+console.log(double(30));
+
+// // // // //　引数を使用した場合
+
+// 与えられた引数numberを２倍し、その値を出力する
+const doubleDouble = (number) =>{
+ console.log( number* 2);
+}
+// 関数を呼び出し、引数として数値を渡す
+doubleDouble(30);
+
+
